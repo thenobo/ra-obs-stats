db32670123c8aa163882c4e7108e1b7024a54c00
thesmaur <97160503+thenobo@users.noreply.github.com>
2022-01-05 10:41:51 +0000

    Make pylint happy

--- ra-obs-stats.py
+++ ra-obs-stats.py
@@ -1,6 +1,5 @@
 from urllib.request import urlopen
 import json
-from datetime import datetime
 import time
 import argparse
 import logging
@@ -17,22 +16,26 @@ parser.add_argument('id')
 args = parser.parse_args()
 PLAYER_ID = args.id
 
-logging.info("Gathering stats for %s" % (PLAYER_ID))
+logging.info('Gathering stats for %s from {BASE_URL}', PLAYER_ID)
 
 while True:
     logging.info("Getting latest games from API")
-    last_games = urlopen("%s/Player/%s/Matches" % (BASE_URL, PLAYER_ID))
+    print(f'{BASE_URL}/Player/{PLAYER_ID}/Matches')
+    last_games = urlopen(f'{BASE_URL}/Player/{PLAYER_ID}/Matches')
     last_games_json = json.loads(last_games.read())
     logging.info("Writing latest games to files")
     map = open("map.txt", "w+", encoding="utf-8")
     opponent = open("opponent.txt", "w+", encoding="utf-8")
     points = open("points.txt", "w+", encoding="utf-8")
     for x in range(0,5):
-        map.write("%s\n" % (last_games_json[x]['mapName']))
+        mapName = last_games_json[x]['mapName']
+        map.write(f'{mapName}\n')
     for x in range(0,5):
-        opponent.write("%s\n" % (last_games_json[x]['opponentName']))
+        opponentName = last_games_json[x]['opponentName']
+        opponent.write(f'{opponentName}\n')
     for x in range(0,5):
-        points.write("%s\n" % (last_games_json[x]['pointsGained']))
+        pointsGained = last_games_json[x]['pointsGained']
+        points.write(f'{pointsGained}\n')
 
     map.close()
     opponent.close()
