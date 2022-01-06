0701a0f817c35c1fcdf72109424b0508d9d1721b
thesmaur <97160503+thenobo@users.noreply.github.com>
2022-01-06 19:18:06 +0000

    Add session stats feature
    Enabled via --session-stats

--- ra-obs-stats.py
+++ ra-obs-stats.py
@@ -18,10 +18,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument('id')
 parser.add_argument('--matches-ticker', dest='matches_ticker', action='store_true')
 parser.add_argument('--player-stats', dest='player_stats', action='store_true')
+parser.add_argument('--session-stats', dest='session_stats', action='store_true')
 args = parser.parse_args()
 PLAYER_ID = args.id
 MATCHES_TICKER_ENABLED = args.matches_ticker
 PLAYER_STATS_ENABLED = args.player_stats
+SESSION_STATS_ENABLED = args.session_stats
 
 def write_ticker_to_file():
     ticker_file_name = "ticker.txt"
@@ -53,7 +55,7 @@ def write_ticker_to_file():
         game_end_time_secs = int(game_start_delta.total_seconds() - last_games_json[x]['matchDuration'])
         game_end_time_mins = int(game_end_time_secs / 60)
 
-        ticker_string = ticker_string + f"{outcome} ({points}) vs {last_games_json[x]['opponentName']} {game_end_time_mins}m ago on {last_games_json[x]['mapName']}  | "
+        ticker_string = ticker_string + f"{outcome} ({points}) vs {last_games_json[x]['opponentName']} {game_end_time_mins}m ago on {last_games_json[x]['mapName']} | "
     
     logging.debug(f"Writing '{ticker_string}' to {ticker_file_name}")
     ticker.write(ticker_string)
@@ -93,17 +95,66 @@ def write_player_stats_to_file():
     logging.debug(f"Closing {ratio_file_name}")
     ratio_file.close()
 
+def write_session_stats_to_file(SESSION_START):
+    session_games_played_file_name = "session_games_played.txt"
+    session_points_change_file_name = "session_points_change.txt"
+    logging.debug(f"Opening {session_games_played_file_name} for writing")
+    session_games_played_file = open(session_games_played_file_name, "w+", encoding="utf-8")
+    logging.debug(f"Opening {session_points_change_file_name} for writing")
+    session_points_change_file = open(session_points_change_file_name, "w+", encoding="utf-8")
+
+    endpoint = f'{BASE_URL}/Player/{PLAYER_ID}/Matches'
+    logging.debug(f"Getting latest matches from {endpoint}")
+    last_games = urlopen(endpoint)
+    last_games_json = json.loads(last_games.read())
+    matches_since_session_start = []
+    first_match_of_session_found = False
+    for match in last_games_json:
+        timestamp = match['starttime']
+        # Some starttimes come back from the API with MS appended, some don't, we don't need them so always remove them
+        if "." in timestamp:
+            timestamp = timestamp.split(".", 1)[0]
+
+        dt_timestamp = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S')
+        if (SESSION_START<dt_timestamp):
+            matches_since_session_start.append(match)
+        elif first_match_of_session_found == True:
+            break
+        else:
+            first_match_of_session_found = True
+            matches_since_session_start.append(match)
+
+    number_of_matches_since_session_start = len(matches_since_session_start)-1
+    most_recent_match = matches_since_session_start[0]
+    first_match_of_session = matches_since_session_start[number_of_matches_since_session_start]
+    
+
+    points_change_since_session_start = int(most_recent_match['playerPoints']-first_match_of_session['playerPoints'])
+    logging.debug(f"Writing {number_of_matches_since_session_start} to {session_games_played_file_name}")
+    session_games_played_file.write(str(number_of_matches_since_session_start))
+    logging.debug(f"Writing {points_change_since_session_start} to {session_points_change_file_name}")
+    session_points_change_file.write(str(points_change_since_session_start))
+    logging.debug(f"Closing {session_games_played_file_name}")
+    session_games_played_file.close()
+    logging.debug(f"Closing {session_points_change_file_name}")
+    session_points_change_file.close()
+
 def main():
     logging.debug('Matches ticker: %s' % (MATCHES_TICKER_ENABLED))
     logging.debug('Player stats: %s' % (PLAYER_STATS_ENABLED))
+    logging.debug('Session stats: %s' % (SESSION_STATS_ENABLED))
     logging.debug('API base: %s' % (BASE_URL))
     logging.debug('Player ID: %s' % (PLAYER_ID))
+    SESSION_START = datetime.utcnow()
+    logging.debug('Session start: %s' % (SESSION_START))
     while True:
         if MATCHES_TICKER_ENABLED == True:
             write_ticker_to_file()
         if PLAYER_STATS_ENABLED == True:
             write_player_stats_to_file()
-
+        if SESSION_STATS_ENABLED == True:
+            write_session_stats_to_file(SESSION_START)
+        
         logging.info(f"Sleeping for {REFRESH_RATE_SECS} seconds")
         time.sleep(REFRESH_RATE_SECS)
 
